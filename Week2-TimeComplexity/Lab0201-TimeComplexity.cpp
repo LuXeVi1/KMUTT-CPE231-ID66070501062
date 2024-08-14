4d0054ac83f0c7790cc3174254df4b820de3cd63
BuXeVi1Workspace <arkkhanirut.pandej@gmail.com>
2024-08-14 23:04:08 +0700

    update Week2

--- Week2-TimeComplexity/Lab0201-TimeComplexity.cpp
+++ Week2-TimeComplexity/Lab0201-TimeComplexity.cpp
@@ -5,25 +5,23 @@
 
 using namespace std;
 
-// Counters for basic operations
-int add_sub_operations = 0;
-int mul_div_operations = 0;
-int comparison_operations = 0;
+// int add_sub_operations = 0;
+// int mul_div_operations = 0;
+// int comparison_operations = 0;
 
-// Recursive function to count valid arrangements
 int countWays(int n, int v, const unordered_set<int>& restricted, vector<bool>& used) {
     if (n == 0) {
-        return 1; // Base case: all students are arranged
+        return 1;
     }
 
     int total_ways = 0;
     for (int i = 1; i <= used.size(); ++i) {
-        comparison_operations++; // for the loop condition
+        // comparison_operations++;
         if (!used[i - 1] && (n != v || restricted.find(i) == restricted.end())) {
-            comparison_operations++; // for the if condition
+            // comparison_operations++;
             used[i - 1] = true;
             total_ways += countWays(n - 1, v, restricted, used);
-            add_sub_operations++; // for the addition
+            // add_sub_operations++;
             used[i - 1] = false;
         }
     }
@@ -31,18 +29,17 @@ int countWays(int n, int v, const unordered_set<int>& restricted, vector<bool>& 
     return total_ways;
 }
 
-// Function to calculate the number of valid arrangements
 int calculateWays(int n, int v, const vector<int>& student_ids, const vector<int>& restricted_ids) {
     unordered_set<int> restricted_positions;
 
     for (int restricted_id : restricted_ids) {
         int position = -1;
         for (int i = 0; i < student_ids.size(); ++i) {
-            comparison_operations++; // for the loop condition
+            // comparison_operations++;
             if (student_ids[i] == restricted_id) {
-                comparison_operations++; // for the if condition
+                // comparison_operations++;
                 position = i + 1;
-                add_sub_operations++; // for the addition
+                // add_sub_operations++;
                 break;
             }
         }
@@ -70,11 +67,6 @@ int main() {
 
     int result = calculateWays(n, v, student_ids, restricted_ids);
     cout << result << endl;
-
-    // Output the count of basic operations
-    cout << "Add/Sub operations: " << add_sub_operations << endl;
-    cout << "Mul/Div operations: " << mul_div_operations << endl;
-    cout << "Comparison operations: " << comparison_operations << endl;
 
     return 0;
 }
