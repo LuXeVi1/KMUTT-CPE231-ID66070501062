996f2a33716c59c05ff19366c2a8c0b29cb4d4ec
Toffy Arkkhanirut Pandej <arkkhanirut.pandej@gmail.com>
2024-09-24 17:03:40 +0700

    update Week6

---
+++ Week6-/Quicksort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int HoarePartition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (true) {
+        do {
+            i++;
+        } while (arr[i] < pivot);
+
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        swap(arr[i], arr[j]);
+    }
+}
+
+// Quicksort Algorithm
+void Quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = HoarePartition(arr, low, high);
+        
+        Quicksort(arr, low, pivot);
+        Quicksort(arr, pivot + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Quicksort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
