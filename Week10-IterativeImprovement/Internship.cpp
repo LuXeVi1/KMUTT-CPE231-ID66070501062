fae6366850800e22593846cca501d6c69a185918
BuXeVi1Workspace <arkkhanirut.pandej@gmail.com>
2024-11-16 20:02:26 +0700

    update Week10-Internship

--- Week10-IterativeImprovement/Internship.cpp
+++ Week10-IterativeImprovement/Internship.cpp
@@ -6,12 +6,14 @@ using namespace std;
 class InternshipMatcher {
 private:
     int n;
-    vector<vector<int> > companyPrefs;
-    vector<vector<int> > studentPrefs;
+    vector<vector<int>> companyPrefs;
+    vector<vector<int>> studentPrefs;
     vector<int> studentMatch;
     vector<int> companyMatch;
-    vector<vector<int> > companyRanks;
-    
+    vector<int> nextProposal;
+    vector<vector<int>> companyRanks;
+    vector<vector<int>> studentRanks;
+
     void computeRankings() {
         companyRanks.resize(n, vector<int>(n));
         for (int i = 0; i < n; i++) {
@@ -19,65 +21,60 @@ private:
                 companyRanks[i][companyPrefs[i][j]] = j;
             }
         }
-    }
-    
-    bool prefersCompany(int s, int c1, int c2) {
-        if (c2 == -1) return true;
-        
-        int pos1 = -1, pos2 = -1;
+
+        studentRanks.resize(n, vector<int>(n));
         for (int i = 0; i < n; i++) {
-            if (studentPrefs[s][i] == c1) pos1 = i;
-            if (studentPrefs[s][i] == c2) pos2 = i;
+            for (int j = 0; j < n; j++) {
+                studentRanks[i][studentPrefs[i][j]] = j;
+            }
         }
-        return pos1 < pos2;
     }
-    
+
 public:
-    InternshipMatcher(int size) : n(size) {
+    InternshipMatcher(int size) : n(size), studentMatch(n, -1), companyMatch(n, -1), nextProposal(n, 0) {
         companyPrefs.resize(n, vector<int>(n));
         studentPrefs.resize(n, vector<int>(n));
-        studentMatch.resize(n, -1);
-        companyMatch.resize(n, -1);
     }
     
-    void setCompanyPreferences(const vector<vector<int> >& prefs) {
+    void setCompanyPreferences(const vector<vector<int>>& prefs) {
         companyPrefs = prefs;
+    }
+    
+    void setStudentPreferences(const vector<vector<int>>& prefs) {
+        studentPrefs = prefs;
         computeRankings();
     }
     
-    void setStudentPreferences(const vector<vector<int> >& prefs) {
-        studentPrefs = prefs;
-    }
-    
     void match() {
-        queue<int> freeStudents;
+        queue<int> freeCompanies;
         for (int i = 0; i < n; i++) {
-            freeStudents.push(i);
+            freeCompanies.push(i);
         }
         
-        while (!freeStudents.empty()) {
-            int s = freeStudents.front();
-            freeStudents.pop();
-            
-            for (int i = 0; i < n && studentMatch[s] == -1; i++) {
-                int c = studentPrefs[s][i];
-                
-                if (companyMatch[c] == -1) {
-                    studentMatch[s] = c;
-                    companyMatch[c] = s;
-                }
+        while (!freeCompanies.empty()) {
+            int company = freeCompanies.front();
+            freeCompanies.pop();
 
-                else if (companyRanks[c][s] < companyRanks[c][companyMatch[c]]) {
-                    freeStudents.push(companyMatch[c]);
-                    studentMatch[s] = c;
-                    studentMatch[companyMatch[c]] = -1;
-                    companyMatch[c] = s;
+            int student = companyPrefs[company][nextProposal[company]++];
+            
+            if (studentMatch[student] == -1) {
+                studentMatch[student] = company;
+                companyMatch[company] = student;
+            } else {
+                int currentCompany = studentMatch[student];
+                if (studentRanks[student][company] < studentRanks[student][currentCompany]) {
+                    studentMatch[student] = company;
+                    companyMatch[company] = student;
+                    companyMatch[currentCompany] = -1;
+                    freeCompanies.push(currentCompany);
+                } else {
+                    freeCompanies.push(company);
                 }
             }
         }
     }
     
-    void printMatches() {
+    void printMatches() const {
         for (int i = 0; i < n; i++) {
             cout << i << " " << studentMatch[i] << endl;
         }
@@ -90,7 +87,7 @@ int main() {
     
     InternshipMatcher matcher(n);
     
-    vector<vector<int> > companyPrefs(n, vector<int>(n));
+    vector<vector<int>> companyPrefs(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> companyPrefs[i][j];
@@ -98,7 +95,7 @@ int main() {
     }
     matcher.setCompanyPreferences(companyPrefs);
     
-    vector<vector<int> > studentPrefs(n, vector<int>(n));
+    vector<vector<int>> studentPrefs(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> studentPrefs[i][j];
